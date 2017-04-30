f70f5bac2116dadb278c3493223144edb2642eca
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-04-29 20:07:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,11 +39,11 @@ def processRequest(req):
     if req.get("result").get("action") != "apiweathertest":
         return {}
     #baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
         return {}
-    #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    #result = urlopen(yql_url).read()
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
