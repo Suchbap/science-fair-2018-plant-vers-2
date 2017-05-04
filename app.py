b6bba9c1d3b8906d1dab71b6f2cecccba01d43f9
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-03 22:23:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + ", My name is Yuvanshu "
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + ", What is the soil moisture of the plant,Yuvanshu? "
 
     print("Response:")
     print(speech)
