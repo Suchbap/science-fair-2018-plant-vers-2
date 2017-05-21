85698b1c9062e65f3768769faffbd5e4a63bfc32
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-21 17:56:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
-
+    print("city name is %s",%city")
     app.run(debug=False, port=port, host='0.0.0.0')
