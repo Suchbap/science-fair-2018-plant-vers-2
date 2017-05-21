361b63fba66b5c583e8f02819b5801f882362bde
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-21 19:31:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+
     print("Request:")
     print(json.dumps(req, indent=4))
 
@@ -33,6 +34,7 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
+
 def processRequest(req):
     if req.get("result").get("action") == "apiweathertest":
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
@@ -41,7 +43,8 @@ def processRequest(req):
             return {}
         yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
         result = urlopen(yql_url).read()
-        data = json.loads(result)        res = makeWebhookResult(data)
+        data = json.loads(result)
+        res = makeWebhookResult(data)
     elif req.get("result").get("action") == "plant-type":
         result = req.get("result")
         parameters = result.get("parameters")
@@ -89,11 +92,11 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + " What is the soil moisture of the plant,Yuvanshu. You live in" + location.get('city')                      
-    
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + " What is the soil moisture of the plant,Yuvanshu. "
+
     print("Response:")
     print(speech)
-   
+
     return {
         "speech": speech,
         "displayText": speech,
@@ -107,4 +110,5 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
+
     app.run(debug=False, port=port, host='0.0.0.0')
