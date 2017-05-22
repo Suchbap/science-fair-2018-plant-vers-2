0d699efb5692544be4f59512a076df0dd7125e64
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-21 20:08:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -92,9 +92,10 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
     
-    if condition > 100:
+    cond = condition.get('temp')
+    if cond > 100:
         speech = "Water the plants"
-    elif condition < 80:
+    elif cond < 80:
         speech = "Donot Water the plants"
 
     #speech = "Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + " What is the soil moisture of the plant,Yuvanshu. "
