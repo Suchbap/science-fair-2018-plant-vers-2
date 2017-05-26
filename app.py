161bff406abdd723ee117f294c2c6ca32ccd60b7
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-26 15:49:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") == "apiweathertest":
+    if req.get("result").get("action") == "plant-city":
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
         yql_query = makeYqlQuery(req)
         if yql_query is None:
