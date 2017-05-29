351ffc0b93ecc2e11b256cc338515b3ef6ad184c
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-29 19:03:56 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -81,6 +81,34 @@ if plant in ["beans","citrus", "cotton", "groundnut", "maize", "sorghum", "soybe
         elif temp <= 60 and fcast in "rainy":
             print("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture percentage is",mois, "%.")
 
+if plant in "corn" and mois<500 and >300:
+if temp >=80 and fcast in ["sunny","cloudy"]:
+         print("Your " + plant + " plant located in the " + location + "  needs water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif temp < 60 and fcast in["rainy", "Cloudy"]:
+            print("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif temp <60 and fcast in"sunny":
+            print ("Your " + plant + " plant located in the " + location + "doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.") if plant in "corn" and mois >=500 and fcast in ["sunny","rainy","cloudy"]:
+            print("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif temp >=60 and <80 and mois<=300 and fcast in["rainy", "Cloudy"]:
+            print("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif temp <60 and mois<=300 and fcast in"sunny":
+            print ("Your " + plant + " plant located in the " + location + " needs water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif temp <=60 and mois <=200 and fcast in "cloudy": 
+            print ("Your " + plant + " plant located in the " + location + " needs water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+    if plant in "wheat" and mois=>350 and <=500:
+    if temp>60 and <80 and fcast in "cloudy":
+            print ("Your " + plant + " plant located in the " + location + "  doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif mois>500 and fcast in["cloudy","rainy"]:
+            print ("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif mois >=200 and <=300 and temp >60 and fcast in "sunny":
+            print ("Your " + plant + " plant located in the " + location + " needs water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif mois<300 and >250 and fcast in "rainy":
+            print ("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+        elif mois <250:
+            print ("Your " + plant + " plant located in the " + location + " needs water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.") 
+        elif mois >800:
+            print ("Your " + plant + " plant located in the " + location + " doesn't need water since the temperature is",temp,"degrees Fahrenheit, the weather forecast is " + fcast + " and the soil moisture is",mois, "ml.")
+
 
 
 
