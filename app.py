eb6c4d5698f0548380b4152df2eca84542d4d10e
yuvanshu <yuvanshu.agarwal21@gmail.com>
2017-05-31 21:29:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 from future.standard_library import install_aliases
@@ -16,6 +17,7 @@ from flask import request
 from flask import make_response
 
 # Flask app should start in global layout
+
 app = Flask(__name__)
 
 
@@ -23,68 +25,71 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    print("Request:")
+    print('Request:')
     print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
+
     # print(res)
+
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-    if req.get("result").get("action") == "water-recommendation":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    if req.get('result').get('action') == 'water-recommendation':
+        result = req.get('result')
+        parameters = result.get('parameters')
+        baseurl = 'https://query.yahooapis.com/v1/public/yql?'
         yql_query = makeYqlQuery(req)
         if yql_query is None:
             return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+        yql_url = baseurl + urlencode({'q': yql_query}) + '&format=json'
         result = urlopen(yql_url).read()
         data = json.loads(result)
         res = makeWebhookResult(data, parameters)
-    elif req.get("result").get("action") == "plant-type":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        plant = parameters.get("plant-type")
+    elif req.get('result').get('action') == 'plant-type':
+        result = req.get('result')
+        parameters = result.get('parameters')
+        plant = parameters.get('plant-type')
         data = {}
-        data["speech"] = "Where are your {0} located? ".format(plant) 
-        data["displayText"] = data["speech"]
-        data["source"] = "apiai-weather-webhook-sample"
+        data['speech'] = 'Where are your {0} located? '.format(plant)
+        data['displayText'] = data['speech']
+        data['source'] = 'apiai-weather-webhook-sample'
         res = data
-    elif req.get("result").get("action") == "plant-location":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        plantlocation = parameters.get("plant-location")
+    elif req.get('result').get('action') == 'plant-location':
+        result = req.get('result')
+        parameters = result.get('parameters')
+        plantlocation = parameters.get('plant-location')
         data = {}
-        data["speech"] = "What is the soil moisture in your plants?"
-        data["displayText"] = data["speech"]
-        data["source"] = "apiai-weather-webhook-sample"
+        data['speech'] = 'What is the soil moisture in your plants?'
+        data['displayText'] = data['speech']
+        data['source'] = 'apiai-weather-webhook-sample'
         res = data
-    elif req.get("result").get("action") == "plant-soil-moisture":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        plantmoisture = parameters.get("number")
+    elif req.get('result').get('action') == 'plant-soil-moisture':
+        result = req.get('result')
+        parameters = result.get('parameters')
+        plantmoisture = parameters.get('number')
         data = {}
-        data["speech"] = "Which city is your plants located in? "
-        data["displayText"] = data["speech"]
-        data["source"] = "apiai-weather-webhook-sample"
+        data['speech'] = 'Which city is your plants located in? '
+        data['displayText'] = data['speech']
+        data['source'] = 'apiai-weather-webhook-sample'
         res = data
     return res
 
 
 def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
+    result = req.get('result')
+    parameters = result.get('parameters')
+    city = parameters.get('geo-city')
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" \
+        + city + "')"
 
 
 def makeWebhookResult(data, parameters):
@@ -94,51 +99,43 @@ def makeWebhookResult(data, parameters):
 
     result = query.get('results')
     if result is None:
-        return {
-            "speech": "No Result",
-            "displayText": "No Result",
-            "source": "apiai-weather-webhook-sample"
-        }
+        return {'speech': 'No Result', 'displayText': 'No Result',
+                'source': 'apiai-weather-webhook-sample'}
     channel = result.get('channel')
     if channel is None:
-        return {
-            "speech": "No Channel",
-            "displayText": "No Channel",
-            "source": "apiai-weather-webhook-sample"
-        }
+        return {'speech': 'No Channel', 'displayText': 'No Channel',
+                'source': 'apiai-weather-webhook-sample'}
 
     item = channel.get('item')
     location = channel.get('location')
     units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {
-            "speech": "No location or item or units",
-            "displayText": "No location or item or units",
-            "source": "apiai-weather-webhook-sample"
-        }
+    if location is None or item is None or units is None:
+        return {'speech': 'No location or item or units',
+                'displayText': 'No location or item or units',
+                'source': 'apiai-weather-webhook-sample'}
 
     condition = item.get('condition')
     if condition is None:
-        return {
-            "speech": "No condition",
-            "displayText": "No condition",
-            "source": "apiai-weather-webhook-sample"
-        }
-        plant = parameters.get("plant-type")
-        moist = parameters.get("SoilMoisture")
+        return {'speech': 'No condition',
+                'displayText': 'No condition',
+                'source': 'apiai-weather-webhook-sample'}
+        plant = parameters.get('plant-type')
+        moist = parameters.get('SoilMoisture')
         cond = float(condition.get('temp'))
-        if plant in "tulips":
+        if plant in 'tulips':
             result = {}
-            result["speech"] = "Yuvanshu your {0} needs water? ".format(plant) 
-            result["displayText"] = result["speech"]
-            result["source"] = "apiai-weather-webhook-sample"
+            result['speech'] = \
+                'Yuvanshu your {0} needs water? '.format(plant)
+            result['displayText'] = result['speech']
+            result['source'] = 'apiai-weather-webhook-sample'
         return result
-
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print("Starting app on port %d" % port)
+    print('Starting app on port %d' % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
+
+			
