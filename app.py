45610cec80754f569c92b7a12f887f1f00ae8ad2
Suchbap <36348934+Suchbap@users.noreply.github.com>
2018-03-04 20:47:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from urllib.error import HTTPError
 
 import json
 import os
+import sqlite3
 
 from flask import Flask
 from flask import request
@@ -36,6 +37,10 @@ def webhook():
     res = json.dumps(res, indent=4)
     print(res)
 
+    # Extract current fcast
+    # curr_fcast = res['query']['results']['channel']['item']['forecast']
+    # curr_fcast_text = curr_fcast['text']
+    
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
@@ -97,19 +102,52 @@ def makeWebhookResult(data, parameters):
         moist = parameters.get('number')
         city = parameters.get('geo-city')
         temp = float(condition.get('temp'))
-        fcast = parameters.get('Forcast')
+        #fcast = parameters.get('Forcast')
+        fcast = condition.get('text')
         decision = ' need '
-        if temp > 70 and moist > 25 :
-             decision = ' does not need '
-        elif temp < 70 and moist < 20 :
-              decision = ' needs '
-        result = {}
-        if plant in ['cotton', 'tulips', 'wheat']:
-            result['speech'] = "Yuvanshu. The temperature is {0} degrees Fahrenheit and the weather forecast is {1} in  {2} and the soil moisture is {3} percent. Based on your data, your  {4}  {5} water  ".format( temp, fcast, city,  moist,  plant, decision )
-            result['displayText'] = result['speech']
-            result['source'] = 'apiai-weather-webhook-sample'
-        return result
+		
+sqlite_file = 'plant_db'    # name of the sqlite database file
+table_name = 'plantdatabase'   # name of the table to be queried
+id_column = 'row_id'
+some_id = 1
+column_1 = 'plant_name'
+column_2 = 'moisture_low'
+column_3 = 'moisture_high' 
+column_4 = 'temp_threshold'   
 
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute("""SELECT plant_name, moisture_low, moisture_high, temp_threshold FROM plantdatabase WHERE plant_name = ?;""", (plant,)) 
+
+for row in c.fetchall():
+    plant_name     = row[0]
+    moisture_low   = row[1]
+    moisture_high  = row[2]
+    temp_threshold = row[3]
+
+# Closing the connection to the database file
+conn.close()
+
+    if plant = plant_name: 
+        if fcast in ["Rain", "Rainy"] :
+            decision = ' does not need '
+        elif moist > moisture_high :
+            decision = ' does not need '
+        elif moist < moisture_low :
+            decision = ' needs '
+        elif moist >= moisture_low and moist <= moisture_high and temp >= temp_threshold and fcast in ["Sunny", "Cloudy", "Partly Cloudy", "Mostly Cloudy", "Partly Sunny", "Mostly Sunny"] :
+            decision = ' needs '
+        elif moist >= moisture_low and moist <= moisture_high and fcast in ["Sunny", "Mostly Sunny", "Partly Sunny"] :
+             decision = ' needs '
+        elif moist >= moisture_low and moist <= moisture_high and temp < temp_threshold and fcast in ["Cloudy", "Partly Cloudy", "Mostly Cloudy"] :
+             decision = ' does not need '      
+        result = {}
+        result['speech'] = "Yuvanshu. The temperature is {0} degrees Fahrenheit and the weather forecast is {1} in  {2} and the soil moisture is {3} percent. Based on your data, your  {4}  {5} water  ".format( temp, fcast, city,  moist,  plant, decision )
+        result['displayText'] = result['speech']
+        result['source'] = 'apiai-weather-webhook-sample'
+    return result
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
@@ -117,3 +155,4 @@ if __name__ == '__main__':
     print('Starting app on port %d' % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
+
